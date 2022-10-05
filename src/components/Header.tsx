97e78ac91f652eed47da73ab02f4d10b6379b350
suhalder <subhobrata.besu@gmail.com>
2022-10-05 18:46:34 -0400

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      My Thoughts
+      <p />
+      <button>New Thought</button>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/SocialPosts.tsx
@@ -0,0 +1,7 @@
+import "./SocialPosts.css";
+
+const SocialPosts = () => {
+  return <div className="SocialPosts">SocialPosts works</div>;
+};
+
+export default SocialPosts;
